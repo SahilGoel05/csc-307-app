75ebc2320697de05135be278e3520ccd3886c030
Sahil Goel <sahilgoe@amazon.com>
2024-04-15 14:31:34 -0700

    Finished IE2 - Added numerous REST APIs that can be queried from the frontend.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,12 +34,67 @@ const users = {
 
 app.use(express.json());
 
+const findUsersByJob = (job) => {
+    return job ?
+        users["users_list"].filter(
+            (user) => user["job"] === job) :
+        users["users_list"];
+}
+const findUserByName = (name) => {
+    return name ?
+        users["users_list"].filter(
+            (user) => user["name"] === name) :
+        users["users_list"];
+};
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+const deleteUser = (id) => {
+    users["users_list"] = users["users_list"].filter((user) => user["id"] !== id);
+};
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
 app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    const job = req.query.job;
+    const nameResult = findUserByName(name)
+    const jobResult = findUsersByJob(job)
+    const finalResult = nameResult.filter((user) => jobResult.includes(user))
+    res.send({ users_list : finalResult});
+});
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+app.get("/users/:name:job", (req, res) => {
+    const name = req.query.x;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
+});
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    deleteUser(id);
+    res.send();
 });
 
 app.listen(port, () => {
